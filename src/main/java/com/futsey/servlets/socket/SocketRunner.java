bddca726718d03849abc44d91d779d188b031d49
Fut <shmopls@gmail.com>
2023-03-13 09:42:10 +0500

    TCP. Класс ServerSocket

--- src/main/java/com/futsey/servlets/socket/SocketRunner.java
+++ src/main/java/com/futsey/servlets/socket/SocketRunner.java
@@ -3,17 +3,21 @@ package com.futsey.servlets.socket;
 import java.io.*;
 import java.net.Inet4Address;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
 
     public static void main(String[] args) throws IOException {
-        var inetAddress = Inet4Address.getByName("google.com");
-        try (Socket socket = new Socket(inetAddress, 80);
+        var inetAddress = Inet4Address.getByName("localhost");
+        try (Socket socket = new Socket(inetAddress, 7777);
              var outputStream = new DataOutputStream(socket.getOutputStream());
              DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
-            outputStream.writeUTF("Hello TCP connect");
-            var response = inputStream.readAllBytes();
-            System.out.println(response.length);
+            var scanner = new Scanner(System.in);
+            while (scanner.hasNextLine()) {
+                var request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: ".concat(inputStream.readUTF()));
+            }
         }
     }
 }
