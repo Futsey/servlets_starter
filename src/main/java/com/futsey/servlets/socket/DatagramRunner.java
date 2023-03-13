7c5b39752f5e35e1a616b9fe40bb3d245c3759e4
Fut <shmopls@gmail.com>
2023-03-13 10:06:41 +0500

    UDP. Класс DatagramSocket

---
+++ src/main/java/com/futsey/servlets/socket/DatagramRunner.java
@@ -0,0 +1,17 @@
+package com.futsey.servlets.socket;
+
+import java.io.IOException;
+import java.net.*;
+
+public class DatagramRunner {
+
+    public static void main(String[] args) throws IOException {
+        var inetAddress = InetAddress.getByName("localhost");
+        try (var datagram = new DatagramSocket()) {
+            var bytes = "Hello from UDP client".getBytes();
+            var packet = new DatagramPacket(bytes, bytes.length, inetAddress, 7777);
+            datagram.send(packet);
+
+        }
+    }
+}
