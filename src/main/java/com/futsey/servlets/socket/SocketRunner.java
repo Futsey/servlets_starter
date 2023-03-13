589890a160c742fbbf3c6a3b23ec1f8878648cf6
Fut <shmopls@gmail.com>
2023-03-13 08:54:22 +0500

    TCP. Класс Socket

--- src/main/java/com/futsey/servlets/socket/SocketRunner.java
+++ src/main/java/com/futsey/servlets/socket/SocketRunner.java
@@ -1,2 +1,19 @@
-package com.futsey.servlets.socket;public class SocketRunner {
+package com.futsey.servlets.socket;
+
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.Socket;
+
+public class SocketRunner {
+
+    public static void main(String[] args) throws IOException {
+        var inetAddress = Inet4Address.getByName("google.com");
+        try (Socket socket = new Socket(inetAddress, 80);
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
+            outputStream.writeUTF("Hello TCP connect");
+            var response = inputStream.readAllBytes();
+            System.out.println(response.length);
+        }
+    }
 }
