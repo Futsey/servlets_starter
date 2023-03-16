6a6492444854556ab9d06f07239e0d9593cf30d3
Fut <shmopls@gmail.com>
2023-03-16 15:32:03 +0500

    HTTP. Servlets. 29. Параметры запроса. Postman

--- src/main/java/com/futsey/servlets/servlet/FirstServlet.java
+++ src/main/java/com/futsey/servlets/servlet/FirstServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -19,10 +20,20 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        var paramValue = req.getParameter("param");
+        var parameterMap = req.getParameterMap();
+        System.out.println();
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "12345");
         try (var writer = resp.getWriter()) {
             writer.write("<h1> Hello from first servlet! </h1>");
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        System.out.println(parameterMap);
     }
 
     @Override
